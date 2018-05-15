c306a08de14dce2a1ceaee6d1a4c7c1fc52f5a68
racinmat <racinmat@fel.cvut.cz>
2018-05-15 03:00:12 +0200

    enhanced calculation scripts

--- UserDatasetGeneratorScripts/calculateAnnotations.py
+++ UserDatasetGeneratorScripts/calculateAnnotations.py
@@ -9,7 +9,7 @@ import pickle
 import time
 
 
-def calculate_annotations(users_file):
+def calculate_annotations(users_file, users_info_file):
     with open(users_file, 'rb') as f:
         users = pickle.load(f)
 
@@ -20,13 +20,16 @@ def calculate_annotations(users_file):
     print('{} score ratings for anime'.format(len([r for u in users if users[u]['loadedRatings'] and users[u]['anime'] is not None for r in users[u]['anime'] if r['my_score'] != '0'])))
     print('{} unique animes'.format(len(set([r['series_animedb_id'] for u in users if users[u]['loadedRatings'] and users[u]['anime'] is not None for r in users[u]['anime']]))))
 
-    with open('UserInfo.rick', 'rb') as f:
-        users = pickle.load(f)
+    with open(users_info_file, 'rb') as f:
+        usersInfo = pickle.load(f)
 
     print('info data')
-    print('{} unique users'.format(len(users)))
-    print('{} of them have annotations'.format(len([u for u in users if users[u]['loadedInfo']])))
+    print('{} unique users'.format(len(usersInfo)))
+    print('{} of them have annotations'.format(len([u for u in usersInfo if usersInfo[u]['loadedInfo']])))
+
+    print('merged data')
+    print('{} of them have all annotations'.format(len([u for u in usersInfo if users[u]['loadedRatings'] and users[u]['anime'] is not None  and usersInfo[u]['loadedInfo']])))
 
 
 if __name__ == '__main__':
-    calculate_annotations('UserList.rick')
+    calculate_annotations('UserList.rick', 'UserInfo.rick')
