14bff144145874d89c6c14194342cce6031f8993
racinmat <racinmat@fel.cvut.cz>
2018-05-18 02:25:30 +0200

    harvesing is ok with being imported

--- UserDatasetGeneratorScripts/getUser.py
+++ UserDatasetGeneratorScripts/getUser.py
@@ -69,83 +69,83 @@ def save_users_mongo(users):
     operations = [pymongo.operations.ReplaceOne(
         filter={"_id": user["_id"]},
         replacement=user,
-        upsert=True
     ) for user in users]
 
     mongo.mal.users.bulk_write(operations)
 
 
-count = 0  # keep count of user for current session
-dataFile = 'UserList.rick'
-mongo = MongoClient('localhost', 27017)
+if __name__ == '__main__':
+    count = 0  # keep count of user for current session
+    dataFile = 'UserList.rick'
+    mongo = MongoClient('localhost', 27017)
 
-changed_users = []  # for mongo
-# users = load_users_pickle()
-users = load_users_mongo()
-for username in users:
-    user = users[username]
+    changed_users = []  # for mongo
+    # users = load_users_pickle()
+    users = load_users_mongo()
+    for username in users:
+        user = users[username]
 
-    if user['loadedRatings']:
-        print('already loaded, skipping')
-        continue
+        if user['loadedRatings']:
+            print('already loaded, skipping')
+            continue
 
-    apiUrl = 'https://kuristina.herokuapp.com/anime/' + username + '.json'  # base url
-    print('Reading {} AnimeList from {}'.format(username, apiUrl))  # console message
+        apiUrl = 'https://kuristina.herokuapp.com/anime/' + username + '.json'  # base url
+        print('Reading {} AnimeList from {}'.format(username, apiUrl))  # console message
 
-    # API call to get JSON
-    page = requests.get(apiUrl)
-    if page.status_code == 503:     # I don't know why, but e.g. https://kuristina.herokuapp.com/anime/purplepinapples.json returns 503 and shuts down the app
-        print('something is broken, fuck')
-        continue
+        # API call to get JSON
+        page = requests.get(apiUrl)
+        if page.status_code == 503:     # I don't know why, but e.g. https://kuristina.herokuapp.com/anime/purplepinapples.json returns 503 and shuts down the app
+            print('something is broken, fuck')
+            continue
 
-    c = page.content
+        c = page.content
 
-    # Decoding JSON
-    jsonData = json.loads(c)
+        # Decoding JSON
+        jsonData = json.loads(c)
 
-    # checking if json data is present
-    if jsonData['myanimelist'] is not None:
-        count += 1
+        # checking if json data is present
+        if jsonData['myanimelist'] is not None:
+            count += 1
 
-        userData = jsonData['myanimelist']
-        user['myinfo'] = userData['myinfo']
-        if 'anime' in userData:
-            # although I could persist all of data, half of every rating is info about anime, which should be kept with anime and not with every rating
-            # so here I just whiteList everything I want
-            user['anime'] = []
+            userData = jsonData['myanimelist']
+            user['myinfo'] = userData['myinfo']
+            if 'anime' in userData:
+                # although I could persist all of data, half of every rating is info about anime, which should be kept with anime and not with every rating
+                # so here I just whiteList everything I want
+                user['anime'] = []
 
-            # this is just mess coming from xml to json conversion, if there is only 1 anime, it is directly dict, not 1item list
-            if type(userData['anime']) is dict:
-                userData['anime'] = [userData['anime']]
+                # this is just mess coming from xml to json conversion, if there is only 1 anime, it is directly dict, not 1item list
+                if type(userData['anime']) is dict:
+                    userData['anime'] = [userData['anime']]
 
-            for anime in userData['anime']:
-                # anime_record = anime_record_to_named_tuple(anime)  # named tuple for for pickle
-                anime_record = anime_record_to_dict(anime)  # classic dict for mongo
-                user['anime'].append(anime_record)
+                for anime in userData['anime']:
+                    # anime_record = anime_record_to_named_tuple(anime)  # named tuple for for pickle
+                    anime_record = anime_record_to_dict(anime)  # classic dict for mongo
+                    user['anime'].append(anime_record)
+            else:
+                user['anime'] = None    # just no data downloaded
+            user['loadedRatings'] = True
+
+            # for mongo
+            changed_users.append(user)
+
+            print('Writing data for {}-th user, {} complete.'.format(count, username))  # console message
         else:
-            user['anime'] = None    # just no data downloaded
-        user['loadedRatings'] = True
+            print(username, 'don\'t have any anime in their list.')
+            # console message for those user who don't have any anime in their list
 
-        # for mongo
-        changed_users.append(user)
+        # just dumping every 500 runs, the rate is about 1000 per hour, so this makes it flush cca each 30 minutes
+        # flushing takes ca 4 minutes for 5 GB
+        if count % 500 == 0:
+            print('{} users processed, persisting them'.format(count))
+            # save_users_pickle(users)
+            save_users_mongo(changed_users)
+            print('dumping done')
+            changed_users = []
 
-        print('Writing data for {}-th user, {} complete.'.format(count, username))  # console message
-    else:
-        print(username, 'don\'t have any anime in their list.')
-        # console message for those user who don't have any anime in their list
+    print('all users processed, persisting them')
+    # save_users_pickle(users)
+    save_users_mongo(changed_users)
+    print('dumping done')
 
-    # just dumping every 500 runs, the rate is about 1000 per hour, so this makes it flush cca each 30 minutes
-    # flushing takes ca 4 minutes for 5 GB
-    if count % 500 == 0:
-        print('{} users processed, persisting them'.format(count))
-        # save_users_pickle(users)
-        save_users_mongo(changed_users)
-        print('dumping done')
-        changed_users = []
-
-print('all users processed, persisting them')
-# save_users_pickle(users)
-save_users_mongo(changed_users)
-print('dumping done')
-
-print('Total', count, 'user data fetched. Done.')
+    print('Total', count, 'user data fetched. Done.')
--- UserDatasetGeneratorScripts/getUserInfo.py
+++ UserDatasetGeneratorScripts/getUserInfo.py
@@ -14,88 +14,63 @@ import json
 import sys
 import myanimelist.session
 from pymongo import MongoClient
+from getUser import *
 
+if __name__ == '__main__':
+    count = 0  # keep count of user for current session
 
-def load_users_pickle():
-    with open(dataFile, 'rb') as f:
-        users = pickle.load(f)
-    return users
+    dataFile = 'UserInfo.rick'
+    mongo = MongoClient('localhost', 27017)
+    session = myanimelist.session.Session()
 
+    changed_users = []  # for mongo
+    # users = load_users_pickle()
+    users = load_users_mongo()
+    for username in users:
+        user = users[username]
 
-def load_users_mongo():
-    users_db = mongo.mal.users
-    return users_db.find()
+        if user['loadedInfo']:
+            print('already loaded, skipping')
+            continue
 
+        print('going to dump data for username {}'.format(username))
+        userData = session.user(username)
 
-def save_users_pickle(users):
-    with open(dataFile, 'wb') as f:
-        pickle.dump(users, f)
+        try:
+            userData.load()
+            count += 1
 
+            # from stats, I only want mean score, total entries, rewatched and episodes, the rest already is scraped with animelists
+            user['loadedInfo'] = True
+            user['info'] = {
+                'gender': userData.gender,
+                'location': userData.location,
+                'birth_date': userData.birthday,
+                'access_rank': userData.access_rank,
+                'join_date': userData.join_date,
+                'last_online': userData.last_online,
+                'stats_mean_score': userData.anime_stats['Mean Score'],
+                'stats_rewatched': userData.anime_stats['Rewatched'],
+                'stats_episodes': userData.anime_stats['Episodes'],
+            }
 
-def save_users_mongo(users):
-    operations = [pymongo.operations.ReplaceOne(
-        filter={"_id": user["_id"]},
-        replacement=user,
-        upsert=True
-    ) for user in users]
+            # for mongo
+            changed_users.append(user)
+        except Exception as e:
+            # raise e   # for debugging parsing
+            pass
 
-    mongo.mal.users.bulk_write(operations)
+        # just dumping every 500 runs, the rate is about 2000 per hour, so this makes it flush cca each 15 minutes
+        if count % 500 == 0:
+            print('{} users processed, persisting them'.format(count))
+            # save_users_pickle(users)
+            save_users_mongo(changed_users)
+            print('dumping done')
+            changed_users = []
 
+    print('all users processed, persisting them')
+    # save_users_pickle(users)
+    save_users_mongo(changed_users)
+    print('dumping done')
 
-count = 0  # keep count of user for current session
-
-dataFile = 'UserInfo.rick'
-mongo = MongoClient('localhost', 27017)
-session = myanimelist.session.Session()
-
-changed_users = []  # for mongo
-# users = load_users_pickle()
-users = load_users_mongo()
-for username in users:
-    user = users[username]
-
-    if user['loadedInfo']:
-        print('already loaded, skipping')
-        continue
-
-    print('going to dump data for username {}'.format(username))
-    userData = session.user(username)
-
-    try:
-        userData.load()
-        count += 1
-
-        # from stats, I only want mean score, total entries, rewatched and episodes, the rest already is scraped with animelists
-        user['loadedInfo'] = True
-        user['info'] = {
-            'gender': userData.gender,
-            'location': userData.location,
-            'birth_date': userData.birthday,
-            'access_rank': userData.access_rank,
-            'join_date': userData.join_date,
-            'last_online': userData.last_online,
-            'stats_mean_score': userData.anime_stats['Mean Score'],
-            'stats_rewatched': userData.anime_stats['Rewatched'],
-            'stats_episodes': userData.anime_stats['Episodes'],
-        }
-
-        # for mongo
-        changed_users.append(user)
-    except Exception as e:
-        # raise e   # for debugging parsing
-        pass
-
-    # just dumping every 500 runs, the rate is about 2000 per hour, so this makes it flush cca each 15 minutes
-    if count % 500 == 0:
-        print('{} users processed, persisting them'.format(count))
-        # save_users_pickle(users)
-        save_users_mongo(changed_users)
-        print('dumping done')
-        changed_users = []
-
-print('all users processed, persisting them')
-# save_users_pickle(users)
-save_users_mongo(changed_users)
-print('dumping done')
-
-print('Total', count, 'user data fetched. Done.')
+    print('Total', count, 'user data fetched. Done.')
